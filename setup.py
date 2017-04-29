1149406228ecc9fed0e58f3c6f477bd463651cfd
ayakushko <ayakushko@intropro.com>
2017-04-30 00:56:16 +0300

    [-] fixed ghost name

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 setup(
   name = 'pyBrowser',
   packages = ['pyBrowser'],
-  version = '0.73',
-  description = 'a collection of simple decorators',
+  version = '0.2',
+  description = 'simple headless browser',
   author = 'Anatolii Yakushko',
   author_email = 'shaddyx@gmail.com',
   url = 'https://github.com/shaddyx/pyBrowser', # use the URL to the github repo
@@ -11,6 +11,6 @@ setup(
   keywords = ['headless', 'browser'], # arbitrary keywords
   classifiers = [],
   install_requires=[
-          'Ghost.py',
+          'ghost.py',
       ]
 )
